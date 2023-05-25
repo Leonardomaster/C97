9bd4aafd1345474f554b2a64bfa96858b482de5a
Leonardomaster <123128747+Leonardomaster@users.noreply.github.com>
2023-05-25 17:30:47 -0300

    Add files via upload

---
+++ game_login (2).js
@@ -0,0 +1,10 @@
+// Crie a função addUser()
+function addUser {
+  // Obtenha o valor do usuário através das ids player1_name_input e player2_name_input
+  player1_name_input
+   player2_name_input
+  // Armazene esses valores localmente
+  window.location(game_page.html)
+  // Atribua "game_page.html" para window.location
+}
+
